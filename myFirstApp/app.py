c699c16957c1fabe7e99f22180ec27571d64da5f
marceloh30 <marceloh30@users.noreply.github.com>
2020-09-30 20:38:41 -0300

    Lo hecho hoy
    -Arreglado la pag. web
    - Meti algunas funciones para obtener datos de las fechas recibidas web

--- myFirstApp/app.py
+++ myFirstApp/app.py
@@ -1,4 +1,6 @@
 import RPi.GPIO as GPIO
+from datetime import date
+from datetime import datetime
 from flask import Flask, render_template, redirect,request, url_for
 app = Flask(__name__)
 GPIO.setmode(GPIO.BCM)
@@ -67,30 +69,58 @@ def tomaDatos():
 		return redirect(url_for('index'))
 	return render_template('parametrosConfi.html')
 
-##Ruta de recepcion de intervalos de tiempo para Temps
+
+##Funcion de busqueda de fechas: Retorna valorNum(temp o lux),Fecha
+def buscarValores(arch,fecha_desde,fecha_hasta):
+	#arch = open("valorTemp.txt" o "valorLux.txt", "r")
+	#Ambos tienen formato: datatime-valorNum
+
+	#Defino variables a llenar
+	valoresNum=[]
+	fechas=[]
+
+	for linea in arch.readlines():
+		valLinea,fechaLinea=linea.split(',')
+		##Verifico si estoy dentro de valores de tiempo
+		if((fecha_desde <= fechaLinea) and (fecha_hasta >= fechaLinea)):
+			fechas.append(fechaLinea)
+			valoresNum.append(valLinea)
+	#Una vez finalizada la lectura cierro arch??
+	##arch.close() 
+
+	return valoresNum,fechas
+
+#Funcion para verificar si el string contiene numeros
+def str_conNums(str_in):
+	return any(char.isdigit() for char in str_in)
+
+##Ruta de recepcion de intervalos de tiempo para Temps-Luxs
 @app.route("/historiaTemp" , methods = ["GET", "POST"])
 def recTiempos():
 	if request.method == 'POST':
-		t1 = request.form['t1']
-		fecha1 = request.form['fecha1']
-		t2 = request.form['t2']
-		fecha2 = request.form['fecha2']
+		t1 = str(request.form['t1'])
+		fecha1 = str(request.form['fecha1'])
+		t2 = str(request.form['t2'])
+		fecha2 = str(request.form['fecha2'])
+		#print(t1,fecha1,"... hasta:",t2,fecha2)
+		#print(str_conNums(t1),str_conNums(t2),str_conNums(fecha1),str_conNums(fecha2))
+		if (str_conNums(t1) and str_conNums(t2) and str_conNums(fecha2) and str_conNums(fecha1)):
+			##Formato- t: '18:06', fecha: '2020-09-01'
+			hm1=t1.split(':')
+			dma1=fecha1.split('-')
+			hm2=t2.split(':')
+			dma2=fecha2.split('-')
+			#args de datetime: Anio, Mes, Dia, Hora, Min, Seg, Miliseg
+			f_desde=datetime(int(dma1[0]),int(dma1[1]),int(dma1[2]),int(hm1[0]),int(hm1[1]),0,0) ##seg y ms los tomo en 0
+			f_hasta=datetime(int(dma2[0]),int(dma2[1]),int(dma2[2]),int(hm2[0]),int(hm2[1]),0,0) ##seg y ms los tomo en 0
+			print("Desde:",f_desde,".. Hasta:",f_hasta,"Ahora a buscarle en archivo")
+			##temps,fechas=buscarValores(archTemp??,f_desde,f_hasta)
+			##Verificar si son nulas o no y chau
+		else:
+			print("Error recibiendo datos")
+		return redirect(url_for('index'))
+	return render_template('askHistTemp.html')
 
-		#print("t1: ",t1,", fecha1:",fecha1,", t2: ",t2,", fecha2: ",fecha2)
-		return redirect(url_for('index'))
-	return render_template('askHistTemp.html')
-'''
-##Ruta de recepcion de intervalos de tiempo para devolver 
-@app.route("/askHistTemp" , methods = ["GET", "POST"])
-def recTiempos():
-	if request.method == 'POST':
-		t1 = request.form['t1']
-		fecha1 = request.form['fecha1']
-		t2 = request.form['t2']
-		fecha2 = request.form['fecha2']
-		print("t1: ",t1,", fecha1:",fecha1,", t2: ",t2,", fecha2: ",fecha2)
-		return redirect(url_for('index'))
-	return render_template('askHistTemp.html')
-'''
+
 if __name__ == "__main__":
    app.run(host='192.168.0.200', port=8080, debug=True)
