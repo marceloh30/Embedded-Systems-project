8d80bdb84d11872afd9dc987aaec3641f39aa662
JulianF120 <julian.ferreira.santurion@gmail.com>
2020-11-11 19:56:57 -0300

    Actualizacion 11/11/2020
    Se agrega lectura Digital
    Falta vincularlo con base de datos, abria que agregar un modelo nuevo para tener digital y analogico separado

---
+++ myFirstApp/lecturaDigital.py
@@ -0,0 +1,38 @@
+import os
+import glob
+import time
+from app import configuraciones, db
+
+ts = 0
+
+os.system('modprobe w1-gpio')
+os.system('modprobe w1-therm')
+ 
+base_dir = '/sys/bus/w1/devices/'
+device_folder = glob.glob(base_dir + '28*')[0]
+device_file = device_folder + '/w1_slave'
+
+def leerTemperatura():
+    f = open(device_file, 'r')
+    lines = f.readlines()
+    f.close()
+    return lines
+ 
+def Valortemp():
+    lines = leerTemperatura()
+    while lines[0].strip()[-3:] != 'YES':
+        time.sleep(0.2)
+        lines = leerTemperatura()
+    equals_pos = lines[1].find('t=')
+    if equals_pos != -1:
+        temp_string = lines[1][equals_pos+2:]
+        temp = float(temp_string) / 1000.0
+        return temp
+
+while True:
+    print(Valortemp())
+    tiempo = db.session.querry(configuraciones).get(1).ts
+    time.sleep(tiempo)
+
+    
+
